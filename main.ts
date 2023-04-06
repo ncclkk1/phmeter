702c972f21c4a2bae6285b136c87024405914922
ncclkk1 <67902759+ncclkk1@users.noreply.github.com>
2023-04-06 16:33:39 +0800

    phmeter 1.0

--- main.ts
+++ main.ts
@@ -1,3 +1,28 @@
-basic.forever(function () {
-	
-})
+
+namespace Newman {
+    /**
+     * ReadVoltage
+     */
+    //% blockId=ReadVoltage block="ReadVoltage"
+    export function ReadVolt(): number {
+        return pins.analogReadPin(AnalogPin.P0) / 1024 * 5000;
+    }
+
+
+    /**
+    * pHValue
+    */
+    //% blockId=pHValue block="Voltage At pH4 %Voltage_pH4|pH7 %Voltage_pH7"
+    //%blockGap=2 weight=1
+    export function pH_Val(Voltage_pH4: number, Voltage_pH7: number): number {
+        let slope = 0;
+        let Numberercept = 0;
+        let phValue = 0;
+
+        slope = (7 - 4) / ((Voltage_pH7 - 1500) / 3 - (Voltage_pH4 - 1500) / 3);
+        Numberercept = 7 - slope * (Voltage_pH7 - 1500) / 3;
+        phValue = slope * ((pins.analogReadPin(AnalogPin.P0) / 1024 * 5000) - 1500) / 3 + Numberercept;
+        return Math.round(phValue);
+    }
+
+}
