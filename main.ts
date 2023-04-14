c4d490263828e1b25de897977a4ba86effff1483
ncclkk1 <67902759+ncclkk1@users.noreply.github.com>
2023-04-14 14:16:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,49 +1,26 @@
-enum MyPort {
-    P0,
-    P1,
-    P2,
-    P3
-}
 namespace Newman {
     /**
      * ReadVoltage
      */
-    //% blockId=ReadVoltage block="電壓 (接腳:%Port v)"
-    export function ReadVolt(Port: MyPort): number {
-        if (Port==0){
-            return pins.analogReadPin(AnalogPin.P0) / 1024 * 5000;
-        } else if (Port == 1){
-            return pins.analogReadPin(AnalogPin.P1) / 1024 * 5000;
-        } else if (Port == 2) {
-                return pins.analogReadPin(AnalogPin.P2) / 1024 * 5000;
-        }else{
-            return pins.analogReadPin(AnalogPin.P3) / 1024 * 5000;
-        }
-        
+    //% blockId=ReadVoltage block="電壓-p1"
+    export function ReadVolt(): number {
+        return pins.analogReadPin(AnalogPin.P1) / 1024 * 5000;
     }
 
    
     /**
     * pHValue
     */
-    //% blockId=pHValue block="pH值 (接腳:%Port)|(pH4:%Voltage_pH4 v)|(pH7:%Voltage_pH7 v)"
+    //% blockId=pHValue block="pH值|(pH4:%Voltage_pH4 v)|(pH7:%Voltage_pH7 v)"
     //%blockGap=2 weight=1
-    export function pH_Val(Port: MyPort, Voltage_pH4: number, Voltage_pH7: number): number {
+    export function pH_Val(Voltage_pH4: number, Voltage_pH7: number): number {
         let slope = 0;
         let Numberercept = 0;
         let phValue = 0;
 
         slope = (7 - 4) / ((Voltage_pH7 - 1500) / 3 - (Voltage_pH4 - 1500) / 3);
         Numberercept = 7 - slope * (Voltage_pH7 - 1500) / 3;
-        if (Port == 0) {
-            phValue = slope * ((pins.analogReadPin(AnalogPin.P0) / 1024 * 5000) - 1500) / 3 + Numberercept;
-        } else if (Port == 1) {
-            phValue = slope * ((pins.analogReadPin(AnalogPin.P1) / 1024 * 5000) - 1500) / 3 + Numberercept;
-        } else if (Port == 2) {
-            phValue = slope * ((pins.analogReadPin(AnalogPin.P2) / 1024 * 5000) - 1500) / 3 + Numberercept;
-        } else {
-            phValue = slope * ((pins.analogReadPin(AnalogPin.P3) / 1024 * 5000) - 1500) / 3 + Numberercept;
-        }
+        phValue = slope * ((pins.analogReadPin(AnalogPin.P1) / 1024 * 5000) - 1500) / 3 + Numberercept;
 
         return Math.round(phValue);
     }
